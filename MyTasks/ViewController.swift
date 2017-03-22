3b60d551bf77e0565ebf29d2c41b031d3c13d32a
Filipe Martins <filipemartins@Filipe-MBP.lan>
2017-03-22 18:55:37 +0000

    initial commit

---
+++ MyTasks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTasks
+//
+//  Created by Filipe Martins on 21/03/2017.
+//  Copyright © 2017 Runtime Revolution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
